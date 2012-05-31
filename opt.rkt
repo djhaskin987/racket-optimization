094a30643378ca65d89d4ca197d730a222f071b0
Daniel Haskin <djhaskin987@gmail.com>
2012-05-30 21:07:48 -0600

    comb through

--- opt.rkt
+++ opt.rkt
@@ -6,38 +6,40 @@
         (* -27 x)
         216)))
 
+(define (bitwise-toggle x pos)
+  (bitwise-xor
+    x
+    (arithmetic-shift 1 pos)))
+
 (define (opt order x len fitness (cmp >))
-  (define (opt-rec ord cur-x cur-fit start end)
+  (let opt-rec ((ord order)
+                (cur-x x)
+                (cur-fit (fitness x))
+                (start 0)
+                (end (- len order)))
     (if (>= start end)
       (values cur-x cur-fit)
-      (let floop ((num-left (- end start))
-                  (current-bit start)
-                  (current-x cur-x)
-                  (current-fit cur-fit))
-        (if (> num-left 0)
-          (let* ((changed-x
-                   (bitwise-xor
-                     current-x
-                     (arithmetic-shift 1 current-bit)))
-                 (changed-fitness (fitness changed-x))
-                 (tried-x
-                   (if (> order 0)
-                     (opt-rec (sub1 order)
-                              changed-x
-                              changed-fitness
-                              (add1 current-bit)
-                              (add1 end))
-                     changed-x))
-                 (tried-fitness (fitness tried-x)))
-            (if (cmp tried-fitness current-fit)
-              (floop (- end start)
-                     (modulo (add1 current-bit) len)
-                     tried-x
-                     tried-fitness)
-              (floop (sub1 num-left)
-                     (modulo (add1 current-bit) len)
-                     current-x
-                     current-fit)))
-          (values current-x
-                  current-fit)))))
-  (opt-rec order x (fitness x) 0 len))
+      (let ((len-rec (- end start)))
+        (let floop ((current-bit start)
+                    (current-x cur-x)
+                    (current-fit cur-fit))
+          (if (>= current-bit end)
+            (values current-x current-fit)
+            (let* ((changed-x
+                     (bitwise-toggle current-x current-bit))
+                   (changed-fitness (fitness changed-x)))
+              (let-values (((tried-x tried-fitness)
+                            (if (> order 0)
+                              (opt-rec (sub1 order)
+                                       changed-x
+                                       changed-fitness
+                                       (add1 current-bit)
+                                       (add1 end))
+                              (values changed-x changed-fitness))))
+                (if (cmp tried-fitness current-fit)
+                  (floop (add1 current-bit)
+                         tried-x
+                         tried-fitness)
+                  (floop (add1 current-bit)
+                         current-x
+                         current-fit))))))))))
