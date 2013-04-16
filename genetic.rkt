592e1581c95037a5a7a649276ab8a34012ba4fd7
Daniel Haskin <djhaskin987@gmail.com>
2013-04-16 08:06:13 -0600

    Fix some bugs, there are yet more

--- genetic.rkt
+++ genetic.rkt
@@ -3,6 +3,7 @@
 (require "utilities.rkt")
 
 (provide
+  long-random
   (contract-out
     (struct solution
             ((bits exact-positive-integer?)
@@ -19,34 +20,43 @@
     (genetic (-> (-> non-negative-integer? number?)
                  exact-positive-integer?
                  exact-positive-integer?
-                 procedure?
                  (-> number? number? boolean?)
+                 procedure?
                  list?))))
 
 (define-struct solution (bits fitness) #:transparent)
 
+(define (long-random (number empty))
+  (if (empty? number)
+    (random)
+    (if (> (integer-length number) 31)
+      (bitwise-ior
+        (arithmetic-shift (random (sub1 (arithmetic-shift 31 1))) 31)
+        (long-random (arithmetic-shift number -31)))
+      (random number))))
+
 ;; Crossover section
-(define (single-crossover len (prng random))
+(define (single-crossover len (prng long-random))
   (sub1 (arithmetic-shift 1 (- len (prng len)))))
 
 
-(define (double-crossover len (prng random))
+(define (double-crossover len (prng long-random))
   (bitwise-xor
     (single-crossover len prng)
     (single-crossover len prng)))
 
-(define (uniform-crossover len (prng random))
+(define (uniform-crossover len (prng long-random))
   (prng (arithmetic-shift 1 len)))
 
 ;; Mutation and Generation
-(define (generate a b n fitness (prng random))
+(define (generate a b n fitness (prng long-random))
   (let ((crossover-mask (single-crossover n prng)))
     (let ((bits (bitwise-ior (bitwise-and (solution-bits a) crossover-mask)
                              (bitwise-and (solution-bits b)
                                           (bitwise-not crossover-mask)))))
       (solution bits (fitness bits)))))
 
-(define (mutate a b n fitness (prng random))
+(define (mutate a b n fitness (prng long-random))
   (let ((probability-of-mutation
           (* 4 (square (1/2 - (/ (- n (count-bits (bitwise-xor a b))) n))))))
     (if (< (prng) (exact->inexact probability-of-mutation))
@@ -54,8 +64,8 @@
         (solution new-bits (fitness new-bits)))
         a)))
 
-(define (genetic fitness n len (prng random) (less-than? <))
-  (let ((population (build-list len (λ (n) (prng len)))))
+(define (genetic fitness n len (less-than? <) (prng long-random))
+  (let ((population (build-list len (λ (n) (prng (arithmetic-shift 1 len))))))
     (let countdown ((times 10000)
                     (pop population))
       (if (zero? times)
@@ -66,19 +76,21 @@
                         (pick-without-replacement 4 population
                                                   (length population)
                                                   prng)))
-            (let ((foe_1 (first chosen))
-                  (foe_2 (second chosen))
+            (let ((foe-1 (first chosen))
+                  (foe-2 (second chosen))
                   (hottie (third chosen))
                   (buff (fourth chosen)))
               (cons
                 ;; change to killing off by rank
-                (if (less-than? (prng) (/ (solution-fitness foe_1)
-                                 (+ (solution-fitness foe_1)
-                                    (solution-fitness foe_2))))
-                  foe_1
-                  foe_2)
+                (if (less-than? (prng) (/ (solution-fitness foe-1)
+                                          (+ (solution-fitness foe-1)
+                                             (solution-fitness foe-2))))
+                  foe-1
+                  foe-2)
                 (cons
                   (generate hottie buff fitness n prng)
                   (cons
                     (mutate hottie buff fitness n prng)
-                    residue))))))))))
+                    (cons
+                      buff
+                      residue)))))))))))
